0b2716a224aa87fef01afc299a73514e9e2574e5
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2022-03-31 19:15:30 -0600

    Environment setup

--- config.mjs
+++ config.mjs
@@ -1,3 +1,28 @@
 /**
- * Create and export configuration variables
+ * *Create and export configuration variables
  */
+
+//* container for all the environment
+
+const environment = {};
+
+// * Staging (default) environment
+environment.staging = {
+    'port' : 3000,
+    'envName' : 'staging',
+    'hashingSecret' : 'secretHashigString'
+}
+
+//* Production environment
+environment.production = {
+    'port' : 5000,
+    'envName' : 'production',
+    'hashingSecret' : 'secretHashigString'
+}
+
+//* Determine which environment was passed as a command-line argument
+let currentEnvironment = typeof( process.env.NODE_ENV ) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+var environmentToExport = '';
+//* Check that the current environment is one of the environments above, if not, default to staging
+export default environmentToExport = typeof( environment[ currentEnvironment ] ) == 'object' ? environment[ currentEnvironment ] : environment.staging;
+
