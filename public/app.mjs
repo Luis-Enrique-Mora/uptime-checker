480552c90d25ee7d0c498ebf9941e7453219ebbc
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2022-06-01 18:51:33 -0600

    load data on check edit page

--- public/app.mjs
+++ public/app.mjs
@@ -467,6 +467,62 @@ app.loadAccountEditPage = function() {
     }
 };
 
+// Load the checks edit page specifically
+app.loadChecksEditPage = function() {
+  // Get the check id from the query string, if none is found then redirect back to dashboard
+    var id = typeof( window.location.href.split( '=' )[ 1 ] ) == 'string' && window.location.href.split( '=' )[ 1 ].length > 0 ? window.location.href.split( '=' )[ 1 ] : false;
+
+    if( id ) {
+        // Fetch the check data
+        var queryStringObject = {
+            'id' : id
+        };
+
+        app.client.request(undefined, 'api/checks', 'GET', queryStringObject, undefined, function( statusCode, responsePayload ) {
+            if(statusCode == 200) {
+                var allChecks = JSON.parse( responsePayload );
+                console.log( allChecks );
+                // Put the hidden id field into both forms
+                var hiddenIdInputs = document.querySelectorAll( "input.hiddenIdInput" );
+                for( var i = 0; i < hiddenIdInputs.length; i++ ){
+                    hiddenIdInputs[ i ].value = allChecks.id;
+                }
+
+                // Put the data into the top form as values where needed
+                document.querySelector( "#checksEdit1 .displayIdInput" ).value = allChecks.id;
+                document.querySelector( "#checksEdit1 .displayStateInput" ).value = allChecks.state;
+                document.querySelector( "#checksEdit1 .protocolInput" ).value = allChecks.protocol;
+                document.querySelector( "#checksEdit1 .urlInput" ).value = allChecks.url;
+                document.querySelector( "#checksEdit1 .methodInput" ).value = allChecks.method;
+                document.querySelector( "#checksEdit1 .timeoutInput" ).value = allChecks.timeoutSeconds;
+                var successCodeCheckboxes = document.querySelectorAll( "#checksEdit1 input.successCodesInput" );
+
+                for( var i = 0; i < successCodeCheckboxes.length; i++ ) {
+                    if( allChecks.successCodes.indexOf( parseInt( successCodeCheckboxes[ i ].value ) ) > -1 ) {
+                        successCodeCheckboxes[ i ].checked = true;
+                    }
+                }
+            } else {
+                // If the request comes back as something other than 200, redirect back to dashboard
+                window.location = '/checks/all';
+            }
+        });
+    } else {
+        window.location = '/checks/all';
+    }
+};
+
+// Loop to renew token often
+app.tokenRenewalLoop = function() {
+    setInterval( function() {
+        app.renewToken( function( err ) {
+            if(!err){
+                console.log("Token renewed successfully @ "+Date.now());
+            }
+        });
+    },1000 * 60 );
+};
+
 app.tokenRenewalToken = function() {
     setInterval( () => {
         app.renewToken( function( error ) {
