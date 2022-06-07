0e6c0b0851054fc5cb81f0fa4e10d74a41c11fbe
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2022-06-06 21:31:32 -0600

    Add CLI initialization to index for start and run CLI

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
 //* Dependencies
 import server from './lib/server.mjs';
 import workers from './lib/workers.mjs';
+import cli from './lib/cli.mjs';
 
 var app = {};
 
 app.init = () => {
     server.init();
     workers.init();
+    //* Start the CLI, and make sure it starts last
+    setTimeout( () => cli.init(), 20 );
 };
 
 app.init();
